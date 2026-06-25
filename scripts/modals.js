92c9afeac2150321d8c2811c6906cb0f1c5e9924
miguel-22z2 <miguelalessilenzi@icloud.com>
2026-06-25 17:30:58 -0300

    fix: adicionado partes finais da estatisticas

--- scripts/modals.js
+++ scripts/modals.js
@@ -4,6 +4,13 @@ const btnAssistencias = document.getElementById("btnAssistencias");
 const modalAssistencias = document.getElementById("dialogAssistencias");
 const closeModalGols = document.getElementById("closeModal");
 const closeModalAssist = document.getElementById("closeModalAssist");
+const btnCartaoA = document.getElementById("btnCartaoA");
+const dialogCartoesA = document.getElementById("dialogCartoesA");
+const btnCloseModalCA = document.getElementById("btnCloseModalCA");
+const btnCartaoV = document.getElementById("btnCartaoV");
+const dialogCartoesV = document.getElementById("dialogCartoesV");
+const btnCloseModalCV = document.getElementById("btnCloseModalCV");
+
 
 btnGols.addEventListener('click', () => {
 
@@ -26,5 +33,29 @@ btnAssistencias.addEventListener('click', () => {
 closeModalAssist.addEventListener('click', () => {
 
     modalAssistencias.close();
+
+});
+
+btnCartaoA.addEventListener('click', () => {
+
+    dialogCartoesA.showModal();
+
+});
+
+btnCloseModalCA.addEventListener('click', () => {
+
+    dialogCartoesA.close();
+
+});
+
+btnCartaoV.addEventListener('click', () => {
+
+    dialogCartoesV.showModal();
+
+});
+
+btnCloseModalCV.addEventListener('click', () => {
+
+    dialogCartoesV.close();
 
 });
